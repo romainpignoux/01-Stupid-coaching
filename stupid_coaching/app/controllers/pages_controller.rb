d94f1a1a6dcd635151de5d1a39d94b5e2dac7004
Romain Pignoux <romain.pignoux@gmail.com>
2015-04-10 16:17:36 +0200

    version finale

--- stupid_coaching/app/controllers/pages_controller.rb
+++ stupid_coaching/app/controllers/pages_controller.rb
@@ -3,6 +3,17 @@ class PagesController < ApplicationController
   end
 
   def answer
-    @query = params[:query]
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
   end
 end
