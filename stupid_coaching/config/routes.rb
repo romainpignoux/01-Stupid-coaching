b7ee884f502f29cc74bad8becb1cf545798fb990
Romain Pignoux <romain.pignoux@gmail.com>
2015-04-10 15:38:40 +0200

    ok

--- stupid_coaching/config/routes.rb
+++ stupid_coaching/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'pages#ask'
+
+  get 'answer', to: 'pages#answer'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
